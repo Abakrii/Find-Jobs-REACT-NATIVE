596dd1610db76e3d0e013656dd97f042a88e0e3f
Abdelrahman Bakry <abdelrahmansalembakry@gmail.com>
2018-12-23 11:17:55 +0200

    1-the-welcome-screen
    "Add Slides to Welcome Screen"

--- src/screens/WelcomeScreen/WelcomeScreen.js
+++ src/screens/WelcomeScreen/WelcomeScreen.js
@@ -1,15 +1,17 @@
 import React, {Component} from 'react';
 import {View, Text} from 'react-native';
+import Slides from '../../components/Slides/Slides'
 
+const SLIDE_DATA =[
+    {text: 'welcome to Job App'},
+    {text :'Use this to get a job'},
+    {text : 'Set your location , then swipe away'}
+]
 
 class WelcomeScreen extends Component {
     render(){
         return(
-            <View>
-                <Text>
-                    WELCOME SCREEN
-                </Text>
-            </View>
+           <Slides data={SLIDE_DATA}/>
         );
     }
 }
