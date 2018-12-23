eb6d6a58a3b628d0654af096ff10e879a6133f6e
Abdelrahman Bakry <abdelrahmansalembakry@gmail.com>
2018-12-23 20:06:25 +0200

    3-crazy-asyncawait-syntax

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -7,7 +7,14 @@ FACEBOOK_LOGIN_SUCCESS
 //AsyncStorage.setItem('fb_token , token);
 //AsyncStorage.getItem('fb_token , token);
 
-export const facebookLogin = ()=>{
+export const facebookLogin = ()=> async dispatch => {
+    let token = await AsyncStorage.getItem('fb_token');
+
+    if(token){
+        // Dispatch an action saying fb login done
+    }else{
+        // Start fb login process
+    }
 
 };
 
